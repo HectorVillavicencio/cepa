0695f398a789be027168033cb9a68d2f05d45350
Hector Villavicnecio <fvillavicencio2@gmail.com>
2024-03-14 00:28:53 -0300

    hice los pages calendarioacademico quienes somos y oferta

---
+++ src/app/domains/products/pages/calendarioacademico/calendarioacademico.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-calendarioacademico',
+  standalone: true,
+  imports: [],
+  templateUrl: './calendarioacademico.component.html',
+  styleUrl: './calendarioacademico.component.css'
+})
+export class CalendarioacademicoComponent {
+
+}
---
+++ src/app/domains/products/pages/ofertaacademica/ofertaacademica.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ofertaacademica',
+  standalone: true,
+  imports: [],
+  templateUrl: './ofertaacademica.component.html',
+  styleUrl: './ofertaacademica.component.css'
+})
+export class OfertaacademicaComponent {
+
+}

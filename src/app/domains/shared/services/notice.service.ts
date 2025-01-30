3b2e9dc5002ef855d4a79a7bfb41d1e45098fede
HectorVillavicencio <fvillavicencio2@gmail.com>
2025-01-30 17:31:15 -0300

    Update notice.service.ts

--- src/app/domains/shared/services/notice.service.ts
+++ src/app/domains/shared/services/notice.service.ts
@@ -13,7 +13,7 @@ export class NoticeService {
   url ='https://graph.instagram.com/me/media?fields=media_url,permalink,caption&access_token=${token}'
   constructor() { }
 
-prueba = 'https://graph.instagram.com/me/media?fields=media_url,permalink,caption&access_token=EAAQcHHxyzWcBOyzCaExORSsKjou3SdIZCzhAuWLA7yeWXHyMjWgYgFsjN9xJNimYbAhZAwADEw7bM1d9rk9mrhG2t2iYoZCpxlBKJpgjxZB4iMLPfGZCP0bZBKasPILUwZB4qtA5Lhd7pwdZBcPpZBHx2NgvMRM2OM7qZCDb2suW0ShorgFvcgsgq4jOZCE'
+prueba = 'https://graph.instagram.com/me/media?fields=media_url,permalink,caption&access_token=EAAQcHHxyzWcBOzxBoUCFjYav8mc3SGPU7BZANkI3JoxqH7OntzBGWUL5iwAiVBVfGJILDWZBh0ZCeOSboFe62ubbZCwcJCVDmRk6GEFdTSXAZCd6YZCPe6MY3RFif5pvn0G4a7J3IbXHDZAMJZBGqSFjqkgvK1ALVGIpGPpZBAfJ6XIJQguA850uY2uKG'
 
   getproduct(){
     return this.http.get<DataModel>(this.prueba);

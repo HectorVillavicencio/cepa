1242df931ff4758395d396361a04ee7ba8c7f70a
Héctor Fabián Villavicencio <103118349+HectorVillavicencio@users.noreply.github.com>
2025-02-10 23:20:31 -0300

    logre poner el token para noticias

--- src/app/domains/shared/services/notice.service.ts
+++ src/app/domains/shared/services/notice.service.ts
@@ -13,7 +13,7 @@ export class NoticeService {
   url ='https://graph.instagram.com/me/media?fields=media_url,permalink,caption&access_token=${token}'
   constructor() { }
 
-prueba = 'https://graph.instagram.com/me/media?fields=media_url,permalink,caption&access_token=EAAQcHHxyzWcBOzxBoUCFjYav8mc3SGPU7BZANkI3JoxqH7OntzBGWUL5iwAiVBVfGJILDWZBh0ZCeOSboFe62ubbZCwcJCVDmRk6GEFdTSXAZCd6YZCPe6MY3RFif5pvn0G4a7J3IbXHDZAMJZBGqSFjqkgvK1ALVGIpGPpZBAfJ6XIJQguA850uY2uKG'
+prueba = 'https://graph.instagram.com/v21.0/me/media?fields=media_url,permalink,caption&access_token=IGAAcazgHdJUpBZAE8zdW5aY2dPRFRnX0hlRGdnTEctWHRhWGtQYVpIbjd3aTJHYl9Cai16SU1mZA1U2c25yMlRudVFfZADBUTlh6elVuTlk5aU5CRU0wM1dXNWh3QWtFZAFdxVFd6TFY3THhQbEkwXzhZAUTFpeWYwQjdJVGM4ekl0TQZDZD'
 
   getproduct(){
     return this.http.get<DataModel>(this.prueba);
